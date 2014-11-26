dc3a4d535ba3db2a9c136cc0046844197c28a050
David Monagle <david.monagle@intrica.com.au>
2014-11-26 16:22:10 +1100

    New way of doing the whole state machine

--- source/state_machine.d
+++ source/state_machine.d
@@ -1,49 +1,45 @@
 ﻿module state_machine;
 
+import std.conv;
+import std.string;
+
 version (Have_vibe_d) {
-	import vibe.data.serialization;
+	pragma(msg, "Compiling d-state-machine with vibe.d support");
 }
 
-struct StateMachineEventAttribute {
-	string stateName;
+struct SMTransitionEventAttribute {
+	string stateEnumName;
 }
 
-struct StateMachineFromAttribute {
-	string[] states;
+SMTransitionEventAttribute transitionEvent(string stateName) {
+	return SMTransitionEventAttribute(stateName);
 }
 
-struct StateMachineToAttribute {
+struct SMBeforeTransitionToAttribute {
 	string state;
 }
 
-@property StateMachineEventAttribute event(string stateName = "state") {
-	return StateMachineEventAttribute(stateName);
+SMBeforeTransitionToAttribute beforeTransitionTo(string state) {
+	return SMBeforeTransitionToAttribute(state);
 }
 
-StateMachineFromAttribute from(string[] states...) {
-	return StateMachineFromAttribute(states);
+struct SMAfterTransitionToAttribute {
+	string state;
 }
 
-StateMachineToAttribute to(string state) {
-	return StateMachineToAttribute(state);
+SMAfterTransitionToAttribute afterTransitionTo(string state) {
+	return SMAfterTransitionToAttribute(state);
 }
 
-
-class InvalidEventException : Exception {
-	this(string s) { super(s); }
+struct SMFromStateAttribute {
+	string[] states;
 }
 
-class InvalidTransitionException : Exception {
-	this(string s) { super(s); }
-}
-
-template PTuple (T...) {
-	alias PTuple = T;
+SMFromStateAttribute fromState(string[] states...) {
+	return SMFromStateAttribute(states);
 }
 
 template camelize(string name) {
-	import std.string;
-
 	immutable string camelize = toLower(name[0..1]) ~ name[1..$];
 }
 
@@ -51,149 +47,241 @@ unittest {
 	assert(camelize!"StatusString" == "statusString");
 }
 
-mixin template StateProperty() {
-	// Template to definte the attribute getter
-	static template defineGetter(StateEnum) {
-		const char[] defineGetter = "const " ~ StateEnum.stringof ~ " " ~ attributeName ~ "() { return _" ~ attributeName ~ "; }";
+import std.traits;
+import std.typetuple : TypeTuple;
+
+template findUDA(UDA, alias Symbol) {
+	private struct UdaSearchResult(alias UDA) {
+		alias value = UDA;
+		bool found = false;
+		long index = -1;
 	}
 
-	// End of templates
-
-	// State Getter
-	version (Have_vibe_d) {
-		@optional @byName @property mixin(defineGetter!StateEnum);
-	} else {
-		@property mixin(defineGetter!StateEnum);
+	private template extract(size_t index, attributes...) {
+		static if (!attributes.length) enum extract = UdaSearchResult!(null)(false, -1);
+		else {
+			static if (is(typeof(attributes[0]) == UDA))
+				enum extract = UdaSearchResult!(attributes[0])(true, index);
+			else 
+				enum extract = extract!(index + 1, attributes[1..$]);
+		}
 	}
 
-	// State Setter
-	@property mixin("void " ~ attributeName ~ "(" ~ StateEnum.stringof ~ " value) { _" ~ attributeName ~ " = value; }");
+	private alias TypeTuple!(__traits(getAttributes, Symbol)) udaTuple;
+	enum findUDA = extract!(0, udaTuple);
 }
 
-mixin template StateMachine(StateEnum) {
-	static immutable string attributeName = camelize!(StateEnum.stringof);
-	// Template to definte private property for the state
-	static template defineStateMember(StateEnum, string attributeName) {
-		const char[] defineStateMember = StateEnum.stringof ~ " _" ~ attributeName ~ ";";
-	}
-
+/// StateProperty 
+struct StateProperty(StateEnum) {
 	private {
-		// define the private variable to hold the state
-		mixin(defineStateMember!(StateEnum, attributeName));
+		StateEnum _state;
 	}
 
-	mixin StateProperty;
+	alias state this;
 
-	/// CTFE transition
-	bool transition(string eventName, this MixinClass)() {
-		import vibe.internal.meta.uda;
+	@property void forceState(StateEnum state) { _state = state; }
+	@property StateEnum state() const {
+		return _state;
+	}
 
-		foreach (memberName; __traits(allMembers, MixinClass)) {
-			if (memberName == eventName) {
-				static if (is(typeof(__traits(getMember, MixinClass.init, memberName)))) {
-					alias member = PTuple!(__traits(getMember, MixinClass, memberName));
-					alias memberType = typeof(__traits(getMember, MixinClass, memberName));
-					alias eventUDA = findFirstUDA!(StateMachineEventAttribute, member);
-					static if (eventUDA.found) {
-						StateEnum returnState = mixin(memberName);
+	@safe string toString() const {
+		return to!string(_state);
+	}
 
-						// TODO: Check to see if the to and from UDAs are there and whether they align
+	version (Have_vibe_d) {
+		import vibe.data.json;
+		import vibe.data.bson;
+		import std.conv;
 
-						mixin("_" ~ attributeName ~ "=returnState;");
-						return true;
-					}
-				}
+		static StateProperty fromJson(Json value) {
+			StateProperty state;
+			if (value.type == Json.Type.string) {
+				state.forceState(to!StateEnum(value.get!string));
 			}
+			return state;
 		}
-		throw new InvalidEventException("No event " ~ eventName ~ " for " ~ MixinClass.stringof);
-	}
+		
+		Json toJson() const {
+			return Json(_state.to!string);
+		}
 
-	/// Allows transitions by string name
-	bool transition(this MixinClass)(string eventName) {
-		import vibe.internal.meta.uda;
-
-		switch (eventName) {
-	        foreach (memberName; __traits(allMembers, MixinClass)) {
-				static if (is(typeof(__traits(getMember, MixinClass.init, memberName)))) {
-					alias member = PTuple!(__traits(getMember, MixinClass, memberName));
-					alias memberType = typeof(__traits(getMember, MixinClass, memberName));
-					alias eventUDA = findFirstUDA!(StateMachineEventAttribute, member);
-					static if (eventUDA.found) {
-						case memberName: return transition!(memberName); 
-					}
-				}
+		static StateProperty fromBson(Bson value) {
+			StateProperty state;
+			if (value.type == Bson.Type.string) {
+				state.forceState(to!StateEnum(value.get!string));
 			}
-			default: {
-				throw new InvalidEventException("No event named " ~ eventName ~ " for class " ~ MixinClass.stringof);
-			}
+			return state;
+		}
+		
+		Bson toBson() const {
+			return Bson(_state.to!string);
 		}
 	}
 }
 
 unittest {
-	import std.exception;
-
-	class Task {
-		enum State {
-			created,
-			todo,
-			closed,
-			cancelled,
-		}
-		
-		mixin StateMachine!(State);
-		
-		@event @from("created") @to("todo") 
-		State makeTodo() {
-			return State.todo;
-		}
-
-		@event @from("todo") @to("cancelled") 
-		State cancel() {
-			return State.cancelled;
-		}
+	enum Status {
+		created,
+		todo,
+		closed,
+		cancelled,
 	}
 	
-	auto t = new Task();
-	assert(t.state == Task.State.created);
-	assertThrown!InvalidEventException(!t.transition!"closed");
-	assert(t.transition!"makeTodo");
-	assert(t.state == Task.State.todo);
-	assert(t.transition("cancel"));
-	assert(t.state == Task.State.cancelled);
-	assertThrown!InvalidEventException(t.transition("wrongEvent"));
+	StateProperty!(Status) status;
+	
+	status.forceState(Status.todo);
+	assert(status == Status.todo);
+}
+
+version (Have_vibe_d) {
+	unittest {
+		import vibe.d;
+		
+		struct Task {
+			enum Status {
+				created,
+				todo,
+				closed,
+				cancelled,
+			}
+			
+			StateProperty!Status status;
+		}
+		
+		Task task;
+		
+		auto toJ = serializeToJson(task);
+		import std.stdio;
+
+		auto jSource = parseJsonString(`{"status": "todo"}`);
+		deserializeJson(task, jSource);
+		import std.stdio;
+		assert (task.status == Task.Status.todo);
+		task.status.forceState(Task.Status.closed);
+		assert (task.status == Task.Status.closed);
+	}
+}
+
+mixin template StateMachine(Parent, StateEnum) {
+	import std.algorithm;
+
+	static immutable string statePropertyName = camelize!(StateEnum.stringof);
+
+	// Create the state member 
+	mixin(StateEnum.stringof ~ " " ~ statePropertyName ~ ";");
+
+	/// Convenience property to set the state by the statePropertyName
+	mixin(`@property void ` ~ statePropertyName ~ `Transition(` ~  StateEnum.stringof ~ ` s) {
+		stateProperty = s;
+	}`);
+
+	/// Convenience property to accept a string state via the prefered property name
+	mixin(`@property void ` ~ statePropertyName ~ `Transition(string s) {
+		stateProperty = s.to!` ~ StateEnum.stringof ~ `;
+	}`);
+
+	@property void stateProperty(StateEnum s) {
+		static if (is(Parent == class) || is(Parent == struct)) {
+			bool checkStates = false;
+			bool transitionSuccessful = true;
+
+			foreach (memberName; __traits(allMembers, Parent)) {
+				static if (is(typeof(__traits(getMember, Parent.init, memberName)) == function)) {
+					string[] fromStates;
+					alias member = TypeTuple!(__traits(getMember, Parent, memberName));
+					alias transitionUDA = findUDA!(SMTransitionEventAttribute, member);
+					static if (transitionUDA.found && (transitionUDA.value.stateEnumName == StateEnum.stringof)) {
+						pragma(msg, "StateMachine event '" ~ memberName ~ "' for '" ~ Parent.stringof ~ "'")
+						alias beforeTransitionUDA = findUDA!(SMBeforeTransitionToAttribute, member);
+						alias afterTransitionUDA = findUDA!(SMAfterTransitionToAttribute, member);
+						static assert(beforeTransitionUDA.found ^ afterTransitionUDA.found, "Must have exactly one of beforeTransitionTo or afterTransitionTo UDA on a @transitionEvent");
+
+						alias fromStateUDA = findUDA!(SMFromStateAttribute, member);
+
+						static if (fromStateUDA.found) {
+							fromStates = fromStateUDA.value.states;
+						}
+						else {
+							fromStates = [];
+						}
+
+						if (fromStates.length == 0 || fromStates.canFind(mixin(statePropertyName).to!string)) {
+							static if (beforeTransitionUDA.found) {
+								if (s == to!StateEnum(beforeTransitionUDA.value.state)) {
+									import std.stdio;
+									writeln("Before: " ~ s.to!string);
+									if (is(ReturnType!member == bool)) {
+										// Execute the transition function and check result
+										if(!mixin(memberName)) {
+											// Need to throw an exception here
+											transitionSuccessful = false;
+											assert(false);
+										}
+
+									}
+									else {
+										// Just execute the transition function
+										mixin(memberName ~ ";");
+									}
+								}
+							}
+							else {
+								if (s == to!StateEnum(afterTransitionUDA.value.state)) {
+									import std.stdio;
+									writeln("After: " ~ s.to!string);
+									// Execute the transition function
+									mixin(memberName ~ ";");
+								}
+							}
+						}
+					}
+				}
+			}
+			// Set the state to the target if the transition was successful
+			if (transitionSuccessful) mixin(statePropertyName) = s;
+		}
+	}
+
 }
 
 unittest {
-	import std.exception;
-	
-	class Task {
+	struct Task {
 		enum Status {
 			created,
 			todo,
 			closed,
 			cancelled,
 		}
-		
-		mixin StateMachine!(Status);
-		
-		@event @from("created") @to("todo") 
-		Status makeTodo() {
-			return Status.todo;
+
+		bool isClosed = false;
+		bool isCancelled = false;
+
+		mixin StateMachine!(Task, Status);
+
+		@transitionEvent("Status") @afterTransitionTo("todo") void afterTodo() {
+			isClosed = false;
+			isCancelled = false;
 		}
-		
-		@event @from("todo") @to("cancelled") 
-		Status cancel() {
-			return Status.cancelled;
+
+		@transitionEvent("Status") @afterTransitionTo("closed") @fromState("todo") void closeCleanup() {
+			isClosed = true;
+		}
+
+		@transitionEvent("Status") @beforeTransitionTo("cancelled") bool cancelledCleanup() {
+			if (isClosed) return false;
+			isCancelled = true;
+			return true;
 		}
 	}
-	
-	auto t = new Task();
-	assert(t.status == Task.Status.created);
-	assertThrown!InvalidEventException(!t.transition!"closed");
-	assert(t.transition!"makeTodo");
-	assert(t.status == Task.Status.todo);
-	assert(t.transition("cancel"));
-	assert(t.status == Task.Status.cancelled);
-	assertThrown!InvalidEventException(t.transition("wrongEvent"));
+
+	Task task;
+	task.status = Task.Status.todo;
+	assert(task.status == Task.Status.todo);
+	task.statusTransition = "closed";
+	assert(task.status == Task.Status.closed);
+	assert(task.isClosed == true);
+	task.statusTransition = "todo";
+	task.statusTransition = "cancelled";
+	assert(task.isCancelled == true);
 }
+
